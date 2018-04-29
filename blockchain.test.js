567e883d95443bc8cb1abc8b3b06cf46ddb07a7c
ThalKod <tmarcelin@thal.tech>
2018-04-29 14:05:02 -0400

    add chain validation test

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require("./blockchain"),
       Block      = require("./block");
 
 describe("Blockchain", ()=>{
-    let blockchain;
+    let blockchain, blockchain2;
 
     beforeEach(()=>{
         blockchain = new Blockchain();
+        blockchain2 = new Blockchain();
     });
 
     it("should start the blockchain with the genesis block", ()=>{
@@ -18,4 +19,24 @@ describe("Blockchain", ()=>{
 
         expect(blockchain.chain[blockchain.chain.length -1].data).toEqual(data);
     });
+
+    it("should validate a valid chain ", ()=>{
+        blockchain2.addBlock("Hello");
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+    });
+
+    it("Should invalidates chain with wrong genesis block", ()=>{
+        blockchain2.chain[0].data = "wrong it";
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it("should invalidate a corupt chain", ()=>{
+        blockchain2.addBlock("Hello");
+        blockchain2.chain[1].data = "weocw ww ";
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
 });
