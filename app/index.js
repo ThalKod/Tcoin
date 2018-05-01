c234f1d658c1b04a645e4d6f82869d33cf893152
ThalKod <tmarcelin@thal.tech>
2018-05-01 12:54:10 -0400

    add POST /mine routes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,23 @@
 const express = require("express"),
+      bodyParser = require("body-parser");
       Blockchain = require("../blockchain");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 var app = express();
+app.use(bodyParser.json());
 
 const blockchain = new Blockchain();
 
+//Get All Blockchain Block
 app.get("/blocks", (req, res)=>{
     res.json(blockchain.chain);
+});
+
+app.post("/mine", (req, res)=>{
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New Block added: ${block.toString()}`);
+    res.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, ()=> console.log(`Listening on Port : ${HTTP_PORT}`));
