fe1124c193e6363d05b747fc88dc62b7249c8b47
ThalKod <tmarcelin@thal.tech>
2018-05-04 09:49:48 -0400

    implement Proof Of Work system

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,43 +1,52 @@
 //Block Class, Containing our Block implementation
 const SHA256 = require("crypto-js").SHA256;
 
+const DIFICULTY = 4;
 
 class Block {
-    constructor(timestamp, previousHash, hash, data){
+    constructor(timestamp, previousHash, hash, data, nonce){
         this.timestamp = timestamp;
         this.previousHash = previousHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
 
     static genesis(){
-        return new this("time", "------", "vjjJDjovsdv", []);
+        return new this("time", "------", "vjjJDjovsdv", [], 0);
     }
 
     static mineBlock(lastBlock, data){
-        const timestamp = Date.now();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp, lastHash, data);
+        let nonce = 0;
 
-        return new this(timestamp, lastHash, hash, data);
+        do{
+            nonce++;
+            timestamp = Date.now();
+            hash = Block.hash(timestamp, lastHash, data,nonce);
+        }while(hash.substring(0,DIFICULTY) !== "0".repeat(DIFICULTY));
+
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static hash(timestamp, lastHash, data){
-        return SHA256(timestamp + lastHash + data).toString();
+    static hash(timestamp, lastHash, data, nonce){
+        return SHA256(timestamp + lastHash + data + nonce).toString();
     }
 
     static blockHash(block){
-        const {timestamp, previousHash, data} = block;
+        const {timestamp, previousHash, data, nonce} = block;
 
-        return Block.hash(timestamp, previousHash, data);
+        return Block.hash(timestamp, previousHash, data, nonce);
     }
 
     toString(){
         return `
         Timestamp    : ${this.timestamp}
         Previous Hash: ${this.previousHash}
-        Hash         : ${this.hash}
+        Hash         : ${this.hash},
+        Nonce        : ${this.nonce},
         Data         : ${this.data}`;
     }
 };
