293824ac02b7b7b580bb8837f3e69829a8c5bed3
ThalKod <tmarcelin@thal.tech>
2018-05-28 13:52:00 -0400

    add transaction test suits

---
+++ test/transation.test.js
@@ -0,0 +1,21 @@
+const Transaction = require("../wallet/transaction");
+const Wallet = require("../wallet/index");
+
+describe("Transaction", ()=>{
+    let transaction, wallet, recipient, amount;
+
+    beforeEach(() =>{
+        wallet = new Wallet();
+        amount = 50;
+        recipient = "recipient address",
+        transaction = Transaction.newTransaction(wallet, recipient, amount);
+    });
+
+    it("should outpout the amount minus the wallet balance", () =>{
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount);
+    });
+
+    it("should outpout the amount added to the recipient", () =>{
+        expect(transaction.outputs.find(output => output.address === recipient).amount).toEqual(amount);
+    });
+});
